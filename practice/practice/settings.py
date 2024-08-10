a7bd9a97f0229561dccd528b43540c9534699ceb
08-zimmad <zimmad.w@gmail.com>
2024-08-10 06:00:19 +0500

    initial commit2

--- practice/practice/settings.py
+++ practice/practice/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -81,8 +81,15 @@ WSGI_APPLICATION = 'practice.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ['DB_NAME'],
+        'USER': os.environ['DB_USER'],
+        'PASSWORD':os.environ['DB_PASSWORD'],
+        'HOST': os.environ['DB_HOST'], 
+        'PORT':  os.environ['DB_PORT'],
+        'TEST':{
+            'NAME': 'test_event_nest'
+        }
     }
 }
 
